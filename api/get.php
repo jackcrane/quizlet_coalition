927d8ce7378da429bfad1725f041014b580b1736
jackcrane <jack-mac@users.noreply.github.com>
2020-09-10 09:43:00 -0400

    Bad practice galore!

--- api/get.php
+++ api/get.php
@@ -6,14 +6,12 @@ $context = stream_context_create(array(
     )
 ));
 
-$title_only;
-
 if(isset($internal)) {
   $url = $internal_url;
   $title_only = $internal_title_only;
 } else {
   $url = $_GET["url"];
-  $title_only = $_GET["title_only"];
+  $title_only = @$_GET["title_only"];
 }
 
 @$contents = file_get_contents($url, false, $context);
