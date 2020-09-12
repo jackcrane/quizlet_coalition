b3589a66cd43f1ea2a19cf26446c4b70dbb9bfd4
jackcrane <jack-mac@users.noreply.github.com>
2020-09-12 10:07:45 -0400

    added stuffs

--- about.php
+++ about.php
@@ -52,7 +52,7 @@
       <p>If you have a quizlet, simply go to the add page and follow the prompts to add your quizlet to our index. It will be made availible to your classmates, sorted by the Quizlet's name, link, class name, and teacher name.</p>
       <p>As an incentive to help build our indexes, when you add your quizlet, there is a chance you will be given an amazon gift card code! That means the more quizlets you upload, the higher chance you have of winning a card!
       </p>
-      <a class="btn blue">Add your quizlet!</a>  <a class="btn qz-yellow">Browse quizlets</a>
+      <a class="btn blue" href="add.php">Add your quizlet!</a>  <a class="btn qz-yellow" href="browse.php">Browse quizlets</a>
       <h1>FAQ's</h1>
       <p><b>Can I use Quizlet Coalition's data?</b></p>
       <p>Kind of. You are permitted to use <i>one</i> of our data and APIs: the qz-data api. <a href="developers.php">Learn more</a>. All other apis are meant only for internal use, and will result in an entire ipban if you use them.</p>
