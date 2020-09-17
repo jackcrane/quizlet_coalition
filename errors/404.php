13d3b70aff3d5dc781ecd6d6f9a760195738c271
jackcrane <jack-mac@users.noreply.github.com>
2020-09-17 19:11:06 -0400

    add the 404 errdoc

---
+++ errors/404.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Quizletcoalition.cc</title>
+	<link href="../../assets/css/style.css" rel="stylesheet">
+  <link href="../../assets/css/pg-spec-template.css" rel="stylesheet">
+	<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-177772435-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-177772435-1');
+</script>
+<meta name="robots" content="noindex">
+<script data-ad-client="ca-pub-5399968831948065" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+</head>
+<body>
+	<div class="maincontent padded">
+		<div class="navbar top clearfix">
+      <a href="../../index.php" style="color:var(--black-rep);"><h2 class="bold left">Quizlet Coalition</h2></a>
+			<div class="right">
+				<a href="about.php" class="link">About</a>
+			</div>
+		</div>
+		<h1 class="thin" style="margin-bottom:0;">Error <span class="thin">404</span></h1>
+    <h5 class="thin" style="margin-top:0;">File Not Found</h5>
+		<h2>We couldn't mask this error.<br><br>You should mask yourself though</h2>
+		<a class="btn blue outline" href="https://www.amazon.com/s?k=masks">Buy a mask</a><br>
+		<a class="btn dark dark-highlights outline" href="/add.php">Add a quizlet</a><br>
+		<a class="btn dark dark-highlights outline" href="/browse.php">Search for a quizlet</a>
+    <div class="footer">
+		</div>
+	</div>
+</body>
+<div id="notify">
+    <div class="ads">
+    </div>
+</div>
+</html>
