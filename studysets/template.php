b52b712a997dadcdb629a1bd7029f720b23683a0
jackcrane <jack-mac@users.noreply.github.com>
2020-09-17 13:20:07 -0400

    still working on an adblock tester

--- studysets/template.php
+++ studysets/template.php
@@ -83,40 +83,8 @@ $qz_base_link = $qz_link_exploded[0] . "/" . $qz_link_exploded[1] . "/" . $qz_li
 		</div>
 	</div>
 </body>
-<script>
-
-async function checkForAdBlocker() {
-  let Blocked;
-
-  async function Request() {
-    try {
-      return fetch(
-        new Request(
-"https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", {
-          method: 'HEAD',
-          mode: 'no-cors'
-        }))
-        .then(function(response) {
-          // There is no AdBlocker
-          Blocked = false;
-          return Blocked;
-        }).catch(function(e) {
-          // Failed, Because of an AdBlocker
-          Blocked = true;
-          return Blocked;
-        });
-    } catch (error) {
-      console.log(error);
-      Blocked = true;
-      return Blocked;
-    }
-  }
-
-  return Blocked !== undefined ? Blocked : await Request();
-}
-
-let usingBlocker = await checkForAdBlocker();
-console.log(usingBlocker)
-
-</script>
+<div id="notify">
+    <div class="ads" style="width:1px">
+    </div>
+</div>
 </html>
