cc5e4c12438637dce7f9f01017d7947e748edd31
jackcrane <jack-mac@users.noreply.github.com>
2020-09-17 13:07:25 -0400

    also added GA hook to index
    At this time, I don't plan to use GA on the index

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 	<meta name="viewport" content="width=device-width,initial-scale=1.0">
 	<!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-177772435-1"></script>
+<script data-ad-client="ca-pub-5399968831948065" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
