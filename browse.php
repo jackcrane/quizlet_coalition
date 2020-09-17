86ca89528ac7b77a657adb334b5d924bf889f86e
jackcrane <jack-mac@users.noreply.github.com>
2020-09-17 13:31:40 -0400

    live debugging with nonsencical php code.... what could go wrong
    oh well... pushing to master anyway!

--- browse.php
+++ browse.php
@@ -1,3 +1,9 @@
+<?php
+
+@$query = $_GET["squery"] . $_GET["tquery"];
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -53,7 +59,7 @@
 			</div>
 		</div>
 		<div class="index-split">
-      <input type="text" class="input dark outline fullwidth" placeholder="Search for quizlets... Search specifically for teachers by putting them in {} or a school with []" onkeyup="search(this.value)">
+      <input type="text" value="<?php @echo $query; ?>" class="input dark outline fullwidth" placeholder="Search for quizlets... Search specifically for teachers by putting them in {} or a school with []" onkeyup="search(this.value)" <?php if($query!=="") echo 'onload="'.$query.'"'; ?>>
       <br><br>
       <div id="results">
         <span class="helpertext">No results</span>
