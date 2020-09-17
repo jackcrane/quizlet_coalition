a9651ef97e372ab22b4cb64da3899f8203803287
jackcrane <jack-mac@users.noreply.github.com>
2020-09-17 13:39:33 -0400

    Update browse.php

--- browse.php
+++ browse.php
@@ -59,7 +59,7 @@
 			</div>
 		</div>
 		<div class="index-split">
-      <input type="text" value="<?php echo $query; ?>" class="input dark outline fullwidth" placeholder="Search for quizlets... Search specifically for teachers by putting them in {} or a school with []" onkeyup="search(this.value)" <?php if($query!=="") {echo 'onload="'.$query.'"';} ?>>
+      <input type="text" value="<?php echo $query; ?>" class="input dark outline fullwidth" placeholder="Search for quizlets... Search specifically for teachers by putting them in {} or a school with []" onkeyup="search(this.value)" <?php if($query!=="") {echo 'onload="search(this.value)"';} ?>>
       <br><br>
       <div id="results">
         <span class="helpertext">No results</span>
